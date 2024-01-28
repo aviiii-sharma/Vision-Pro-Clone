ae517952f6358fa551dec4b3f48dff4257883b76
aviiii-sharma <aviral7703@gmail.com>
2024-01-28 11:43:24 +0530

    Animated First Page

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ gsaploco();
 gsap.to("#page>video", {
   scrollTrigger:{
     trigger:'#overview.video',
-    start: '3% top',
+    start: '2% top',
     end: 'bottom top',
     markers: true,
     scroller:'#main'
@@ -35,4 +35,25 @@ gsap.to("#page>video", {
   onStart: () => {
     document.querySelector('#overview>video').play()
   }
+})
+
+gsap.to ("#overview",{
+  scrollTrigger:{
+    trigger:'#overview',
+    start: 'top top',
+    end: 'bottom top',
+    scroller:'#main',
+    pin: true
+  }
+})
+
+gsap.to ("#page-bottom",{
+  scrollTrigger:{
+    trigger:'#page-bottom',
+    start: 'top 60%',
+    end: 'bottom top',
+    scroller:'#main',
+    scrub: 0.5
+  },
+  opacity: 0
 })
